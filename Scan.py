705480221e774ca966c758bf4bb3a09daf2e694a
FilhodEmer <emerson_rodrigues_c@outlook.com>
2022-06-23 22:09:58 -0300

    Files checking

--- Scan.py
+++ Scan.py
@@ -1,6 +1,6 @@
 import re, os, sys, platform, glob, pathlib
 
-with open('NOTES.txt', 'r', encoding='UTF-8') as file:
+'''with open('NOTES.txt', 'r', encoding='UTF-8') as file:
     content = file.readlines()
 
     print(content) #The entire file
@@ -8,14 +8,24 @@ with open('NOTES.txt', 'r', encoding='UTF-8') as file:
     for line in content:
         if re.search('de', line):
             print(line, re.findall('de', line))
-
+'''
 '''find = re.compile(r'de')
 print(find.search(line))
 print(find.findall(line))
 print(find.sub('of', line))'''
 
-print(os.listdir('.'))
+'''print(os.listdir('.'))
 print(platform.uname())
 print(sys.platform)
 print(glob.glob('*.py'))
 print(pathlib.Path.cwd())
+
+help('modules')'''
+
+arq = os.chdir('c:/Users/emers/Desktop/Faculdade/TCC/Scan_DataDiscovery')
+for file in glob.glob('*.txt'):
+    #print(file)
+    for info in open(file, encoding='UTF-8'):
+        #print(info)
+        if 'Abra' in info:
+            print(file)
