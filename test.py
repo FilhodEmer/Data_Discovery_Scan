63ece1200bae52df1147d89b0f1699906c7e7c20
FilhodEmer <emerson_rodrigues_c@outlook.com>
2022-07-04 22:11:48 -0300

    Fix'd

--- test.py
+++ test.py
@@ -1,11 +1,6 @@
-aux = dict()
+import pathlib, docx2txt
 
-key = 'Diretório'
-for i in range(10):
-    if key not in aux:
-        aux[key] = str(i)
-    else:
-        aux[key] = temp, str(i)
-    temp = str(i)
-
-print(aux)
+for file in list(pathlib.Path('.').rglob('*.*')):
+    if file.suffix == '.docx':
+        print('Yes!')
+        document = docx2txt.process('teste.docx')
